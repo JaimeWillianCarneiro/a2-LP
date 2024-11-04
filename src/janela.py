4f72cb2b81fa66f4e76552e20188cab4c71ead80
JaimeWillianCarneiro <jwcs2@aluno.ifal.edu.br>
2024-11-04 20:46:39 -0300

    adicionando arquivo de exemplo inicial

---
+++ src/janela.py
@@ -0,0 +1,20 @@
+import pygame
+from pygame.locals import *
+from sys import exit
+
+
+pygame.init()
+
+LARGURA = 640
+ALTURA = 480
+
+tela = pygame.display.set_mode((LARGURA, ALTURA))
+pygame.display.set_caption('Scooby Doo e A Caçada Cibernética')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+    pygame.draw.rect(tela, (255, 0, 0), (200, 300, 40, 50))
+    pygame.display.update()
