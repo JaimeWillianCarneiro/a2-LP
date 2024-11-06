8b90053ceb21c898682d71f80a653cbba98737df
JaimeWillianCarneiro <jwcs2@aluno.ifal.edu.br>
2024-11-05 23:00:21 -0300

    adicionando alterações no branch jaime

--- src/janela.py
+++ src/janela.py
@@ -17,4 +17,8 @@ while True:
             pygame.quit()
             exit()
     pygame.draw.rect(tela, (255, 0, 0), (200, 300, 40, 50))
+    pygame.draw.circle(tela, (0, 0, 255), (300, 200), 30)
+    pygame.draw.line(tela, (255, 255, 0), (390, 0), (390, 600), 5)
+    
+    
     pygame.display.update()
