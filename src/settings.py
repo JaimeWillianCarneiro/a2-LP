30fe2186bb8ee4ec6856956210167ce4d38b1c5a
JaimeWillianCarneiro <jwcs2@aluno.ifal.edu.br>
2024-11-20 23:48:40 -0300

    add classe Menu

--- src/settings.py
+++ src/settings.py
@@ -6,7 +6,10 @@ SCREEN_DIMENSIONS = (pg.display.Info().current_w, pg.display.Info().current_h)
 GAME_TITLE = 'SCOOBY DOO'
 
 START_SOUND_MENU =  'assets/sounds/backmusic.mp3'
-START_BACKGROUNG_MENU= 'assets/title screen/title screen.png'
+START_BACKGROUND_MENU= 'assets/title screen/title screen.png'
+START_COLUMNS_MENU = 12
+START_ROWS_MENU = 20
+
 class Fonts(Enum):
     PLAYER_NAME = pg.font.SysFont('comicsansms', 30, True, False)
     PLAYER_LIFE = pg.font.SysFont('franklingothicheavy', 25, True, False)
